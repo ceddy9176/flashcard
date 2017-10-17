6c018cb3fb9157d5fa88d86f45fc01bdc34f1e2d
Chris Johnson and Connor miller <chris@>
2017-10-17 14:26:59 -0700

    add function to toggle displayed info

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("h3").click(function(){
+    $(this).next().toggle();
+    $(this).toggle("definition");
+  })
+});
+
+$(function() {
+  $(".definition").click(function() {
+    $(this).hide();
+    $("h3").show();
+  });
+});
+
+$(function() {
+  $(".list-group-item").flip();
+});
